e8b29c5964096552c27ade6ffd242fe6229efafc
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-12-01 16:22:38 +0600

    Problem 1: calculateTax()

---
+++ JS-ProblemSolving-Ass4/problem1.js
@@ -0,0 +1,20 @@
+/**
+ * ? Problem 01: Write a function called calculateTax(), which will calculate 20% tax for monthly income after subtract monthly expenses.
+ * ! Conditions: income and expenses should be positive number and income should be greater then or equal to expenses.
+ */
+
+function calculateTax(income, expenses) {
+  let restIncome = 0;
+  let incomeTax = 0;
+
+  if (income >= 0 && expenses >= 0 && income >= expenses) {
+    restIncome = income - expenses;
+    incomeTax = (restIncome * 20) / 100; // restIncome * 0.20;
+    return incomeTax;
+  } else {
+    return "Invalid Input";
+  }
+}
+
+const income_tax = calculateTax(6000, 15000);
+console.log("Income Tax : ", income_tax);
