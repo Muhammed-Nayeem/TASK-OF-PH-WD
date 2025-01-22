f56832fd6a65522ad9e72970b76022805082feba
Muhammed Nayeem <mohammednayeem808@gmail.com>
2025-01-23 00:06:05 +0600

    Donation and History Functionalities

--- Donate-Bangladesh-Ass5/Assets/JavaScript/app.js
+++ Donate-Bangladesh-Ass5/Assets/JavaScript/app.js
@@ -1,3 +1,8 @@
+//Donation Calculation Function:
+const donationMoneyHandlerOne = document.getElementById("cd_btn-one");
+const donationMoneyHandlerTwo = document.getElementById("cd_btn-two");
+const donationMoneyHandlerThree = document.getElementById("cd_btn-three");
+
 //Donation Button Event Handler:
 document.getElementById("donation_btn").addEventListener("click", function(e) {
   showCategoriesOption("donation");
@@ -8,4 +13,26 @@ document.getElementById("donation_btn").addEventListener("click", function(e) {
 document.getElementById("history_btn").addEventListener("click", function(e) {
   showCategoriesOption("history");
   activeButton("donation_btn");
+});
+
+//Donation Button Event Handlers:
+donationMoneyHandlerOne.addEventListener("click", function(e) {
+  e.preventDefault();
+  let donationMoney = getInputValueById("cd_amount-one");
+  let currentMoney = getTextFieldValueById("cdb-one");
+  let currentTotal = getTextFieldValueById("total_balance");
+  let donationTitle = getTitleById("cd_title-one");
+
+  if(!isNaN(donationMoney) && donationMoney > 0 && donationMoney <= currentTotal) {
+    let newCurrentBalance = currentMoney + donationMoney;
+    let newCurrentTotal = currentTotal - donationMoney;
+    setTextValueById("cdb-one", "total_balance", newCurrentBalance, newCurrentTotal);
+    transactionSummary(donationTitle, donationMoney);
+    alert("Donation Successful!");
+    clearInputFieldById("cd_amount-one");
+  } else if(currentTotal === 0) {
+    alert("You do not have sufficient balance!");
+  } else {
+    alert("Invalid Number!");
+  }
 });
--- Donate-Bangladesh-Ass5/Assets/JavaScript/utilities.js
+++ Donate-Bangladesh-Ass5/Assets/JavaScript/utilities.js
@@ -1,4 +1,35 @@
-//Common Function: Donation & History
+//Common Function: To Get Input Value
+function getInputValueById(id) {
+  let inputFieldValue = document.getElementById(`${id}`).value;
+  let inputFieldNumber = parseFloat(inputFieldValue);
+  return inputFieldNumber;
+}
+
+//Common Function: To Get Text Field Value
+function getTextFieldValueById(id) {
+  let textFieldValue = document.getElementById(`${id}`).innerText;
+  let textFieldNumber = parseFloat(textFieldValue);
+  return textFieldNumber;
+}
+
+//Common Function: To Get Cause Title
+function getTitleById(id) {
+  let causeTitle = document.getElementById(`${id}`).innerText;
+  return causeTitle;
+}
+
+//Common Function: To Set Value
+function setTextValueById(id_1, id_2, currentBalance, totalBalance) {
+  document.getElementById(`${id_1}`).innerText = currentBalance;
+  document.getElementById(`${id_2}`).innerText = totalBalance;
+}
+
+//Common Function: To Clear Input Field
+function clearInputFieldById(id) {
+  document.getElementById(`${id}`).value = "";
+}
+
+//Common Function: Donation & History Option
 function showCategoriesOption(id) {
   document.getElementById("donation").classList.add("hidden");
   document.getElementById("history").classList.add("hidden");
@@ -10,4 +41,17 @@ function activeButton(id) {
   document.getElementById("donation_btn").classList.add("btn-active", "transition-all");
   document.getElementById("history_btn").classList.add("btn-active", "transition-all");
   document.getElementById(`${id}`).classList.remove("btn-active");
+}
+
+//Generate History Elements:
+function transactionSummary(donation_title, donation_money) {
+  let date = new Date();
+  let transactionContainer = document.getElementById("history_container");
+  let historyDiv = document.createElement("div");
+  historyDiv.className = "border px-5 py-5 rounded";
+  historyDiv.innerHTML = `
+    <h2 class="text-xl text-black, font-bold mb-2">${donation_money} Taka is ${donation_title}</h2>
+    <p class="text-sm text-gray-500">Date: ${date.toDateString()} ${date.toTimeString()}</p>
+  `;
+  transactionContainer.appendChild(historyDiv);
 }
