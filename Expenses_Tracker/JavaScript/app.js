4f7f566d7c657be2b89a332edb8c91941eca03fc
Muhammed Nayeem <mohammednayeem808@gmail.com>
2025-02-02 21:20:15 +0600

    realtime validation and functionalities

--- Expenses_Tracker/JavaScript/app.js
+++ Expenses_Tracker/JavaScript/app.js
@@ -1,25 +1,97 @@
 //Clickable Buttons:
 const calculateExpenseButton = document.getElementById("calculate");
 const calculateSavingsButton = document.getElementById("calculate-savings");
+const assistantTabButton = document.getElementById("assistant-tab");
+const historyTabButton = document.getElementById("history-tab");
 
 //Event Listener Of calculateExpenseButton:
-calculateExpenseButton.addEventListener("click", function() {
+calculateExpenseButton.addEventListener("click", function(e) {
   const allValues_obj = getInputValue("#income", "#software", "#courses", "#internet");
   const {incomeValue, softwareValue, coursesValue, internetValue} = allValues_obj;
   const totalExpenses = softwareValue + coursesValue + internetValue;
   const totalBalance = incomeValue - totalExpenses;
-  setTextFieldValue("total-expenses", "balance", totalExpenses, totalBalance);
-  showSection("results");
+
+  if (incomeValue < 0 || isNaN(incomeValue)) {
+    document.getElementById("income-error").classList.remove("hidden");
+    return;
+  } if (softwareValue < 0 || isNaN(softwareValue)) {
+    document.getElementById("software-error").classList.remove("hidden");
+    return;
+  } if (coursesValue < 0 || isNaN(coursesValue)) {
+    document.getElementById("courses-error").classList.remove("hidden");
+    return;
+  } if (internetValue < 0 || isNaN(internetValue)) {
+    document.getElementById("internet-error").classList.remove("hidden");
+    return;
+  } else {
+    document.getElementById("income-error").classList.add("hidden");
+    document.getElementById("software-error").classList.add("hidden");
+    document.getElementById("courses-error").classList.add("hidden");
+    document.getElementById("internet-error").classList.add("hidden");
+    if (totalExpenses > incomeValue) {
+      document.getElementById("logic-error").classList.remove("hidden");
+      return;
+    } else {
+      document.getElementById("logic-error").classList.add("hidden");
+      setTextFieldValue("total-expenses", "balance", totalExpenses,totalBalance);
+      showSection("results");
+      generateHistory(incomeValue, totalExpenses, totalBalance);
+    }
+  }
 });
 
 //Event Listener Of calculateSavingsButton:
-calculateSavingsButton.addEventListener("click", function() {
+calculateSavingsButton.addEventListener("click", function(e) {
   const allValues_obj = getInputValue("#income", "#software", "#courses", "#internet");
   const {incomeValue, softwareValue, coursesValue, internetValue} = allValues_obj;
-  let savingsAmount = getInputValueBySelector("#savings");
   const totalExpenses = softwareValue + coursesValue + internetValue;
   const totalBalance = incomeValue - totalExpenses;
-  const savings = (totalBalance * savingsAmount) / 100;
-  const remainingBalance = totalBalance - savings;
-  setTextFieldValue("savings-amount", "remaining-balance", savings, remainingBalance);
+  let savingsAmount = getInputValueBySelector("#savings");
+
+  if (savingsAmount < 0 || isNaN(savingsAmount)) {
+    document.getElementById("savings-error").classList.remove("hidden");
+    return;
+  } else {
+    document.getElementById("savings-error").classList.add("hidden");
+    const savings = (totalBalance * savingsAmount) / 100;
+    const remainingBalance = totalBalance - savings;
+    setTextFieldValue("savings-amount", "remaining-balance", savings, remainingBalance);
+  }
+});
+
+//Show and Hide Categories Event Listener:
+assistantTabButton.addEventListener("click", function(e) {
+  showCategories("expense-form");
+  buttonActivation("assistant-tab");
+});
+
+historyTabButton.addEventListener("click", function(e) {
+  showCategories("history-section");
+  buttonActivation("history-tab");
+}); 
+
+//For Instant Validations:
+document.getElementById("income").addEventListener("input", function(e) {
+  const incomeValue = getInputValueBySelector("#income");
+  realtimeInputValidation("income-error", incomeValue);
+});
+
+document.getElementById("software").addEventListener("input", function(e) {
+  const softwarePrice = getInputValueBySelector("#software");
+  realtimeInputValidation("software-error", softwarePrice);
+});
+
+document.getElementById("courses").addEventListener("input", function(e) {
+  const coursesPrice = getInputValueBySelector("#courses");
+  realtimeInputValidation("courses-error", coursesPrice);
+});
+
+document.getElementById("internet").addEventListener("input", function(e) {
+  const internetBill = getInputValueBySelector("#internet");
+  realtimeInputValidation("internet-error", internetBill);
+});
+
+document.getElementById("savings").addEventListener("input", function(e) {
+  const savingsAmount = getInputValueBySelector("#savings");
+  realtimeInputValidation("savings-error", savingsAmount);
 });
--- Expenses_Tracker/JavaScript/utilities.js
+++ Expenses_Tracker/JavaScript/utilities.js
@@ -1,3 +1,16 @@
+let count = 0;
+
+//Common Function For Instant Validation:
+function realtimeInputValidation(id, input_value) {
+  if (input_value < 0 || isNaN(input_value)) {
+    document.getElementById(id).classList.remove("hidden");
+    return;
+  } else {
+    document.getElementById(id).classList.add("hidden");
+    return;
+  }
+}
+
 //Common Function For Get Input Values:
 function getInputValue(selector_1, selector_2, selector_3, selector_4) {
   let incomeValue = parseFloat(document.querySelector(selector_1).value);
@@ -23,6 +36,41 @@ function setTextFieldValue(elementID_1, elementID_2, totalExpense, totalBalance)
 
 //Common Function For Show & Hide Sections:
 function showSection(id) {
-  document.getElementById(id).classList.add("hidden");
+  document.getElementById("results").classList.add("hidden");
   document.getElementById(id).classList.remove("hidden");
+}
+
+//Common Function For Categories Hide & show:
+function showCategories(id) {
+  document.getElementById("expense-form").classList.add("hidden");
+  document.getElementById("history-section").classList.add("hidden");
+  document.getElementById(id).classList.remove("hidden");
+}
+
+//Common Function For Button Activation:
+function buttonActivation(id) {
+  if (id === "assistant-tab") {
+    document.getElementById(id).classList.add("bg-gradient-to-r", "from-green-600", "to-green-400", "text-white");
+    document.getElementById("history-tab").classList.remove("bg-gradient-to-r", "from-green-600", "to-green-400", "text-white");
+  } else {
+    document.getElementById(id).classList.add("bg-gradient-to-r", "from-green-600", "to-green-400", "text-white");
+    document.getElementById("assistant-tab").classList.remove("bg-gradient-to-r", "from-green-600", "to-green-400", "text-white");
+    document.getElementById("assistant-tab").classList.add("text-gray-600");
+  }
+}
+
+//Utility Function For History Generate:
+function generateHistory(income, expense, balance) {
+  count += 1;
+  const historyContainer = document.getElementById("history-list");
+  let div = document.createElement("div");
+  div.classList.add("mt-4", "from-blue-50", "to-purple-50", "p-4", "rounded-lg", "shadow-md", "border-l-2", "border-blue-500");
+  div.innerHTML = `
+  <p class="text-gray-500 text-sm font-bold">Serial: ${count}</p>
+  <p class="text-gray-500 text-sm font-semibold">Date: ${new Date().toLocaleDateString()}</p>
+  <p class="text-gray-500 text-sm font-semibold">Income: ${income}৳</p>
+  <p class="text-gray-500 text-sm font-semibold">Expense: ${expense}৳</p>
+  <p class="text-gray-500 text-sm font-semibold">Balance: ${balance}৳</p>
+  `;
+  historyContainer.insertBefore(div, historyContainer.firstChild);
 }
