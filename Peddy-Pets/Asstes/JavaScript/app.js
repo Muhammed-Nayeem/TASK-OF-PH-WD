95773d7ca76692c8b9fc0a93bfc5077872abf0f0
Muhammed Nayeem <mohammednayeem808@gmail.com>
2025-05-18 03:06:11 +0600

    All Pets Load with API Endpoint

--- Peddy-Pets/Asstes/JavaScript/app.js
+++ Peddy-Pets/Asstes/JavaScript/app.js
@@ -9,6 +9,17 @@ const loadCategories = async() => {
   }
 };
 
+//Load All Pets:
+const loadAllPets = async() => {
+  try {
+    let response = await fetch("https://openapi.programming-hero.com/api/peddy/pets");
+    let data = await response.json();
+    displayPets(data.pets);
+  } catch(e) {
+    console.log(e);
+  }
+};
+
 //Display Categories:
 const displayCategories = (categories) => {
   let categoriesContainer = document.getElementById("categories-container");
@@ -24,5 +35,51 @@ const displayCategories = (categories) => {
   });
 };
 
+//Display Pets:
+const displayPets = (pets) => {
+  let petsContainer = document.getElementById("pets-container");
+  petsContainer.innerHTML = "";
+
+  pets.map((pet) => {
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "border", "border-gray-200", "rounded-md", "p-3");
+    cardDiv.innerHTML = `
+    <figure>
+      <img src="${pet?.image}" alt="${pet?.pet_name || "pet"}" class="rounded-md w-full h-[180px] object-cover" />
+    </figure>
+    <div class="mt-2">
+      <h4 class="text-lg font-extrabold text-black">${pet?.pet_name || "N/A"}</h4>
+      <ul class="mt-2 mb-3">
+        <li class="text-gray-600 flex items-center gap-1 mb-1 text-base">
+          <i class="fa-regular fa-snowflake"></i>
+          <span>Breed: ${pet?.breed || "N/A"}</span>
+        </li>
+        <li class="text-gray-600 flex items-center gap-1 mb-1 text-base">
+          <i class="fa-regular fa-calendar"></i>
+          <span>Birth: ${pet?.date_of_birth || "N/A"}</span>
+        </li>
+        <li class="text-gray-600 flex items-center gap-1 mb-1 text-base">
+          <i class="fa-solid fa-mercury"></i>
+          <span>Gender: ${pet?.gender || "N/A"}</span>
+        </li>
+        <li class="text-gray-600 flex items-center gap-1 mb-1 text-base">
+          <i class="fa-solid fa-dollar-sign"></i>
+          <span>Price: ${pet?.price ? `${pet.price}$` : "N/A"}</span>
+        </li>
+      </ul>
+      <div class="flex justify-between items-center border-t border-gray-200">
+        <button class="mt-4 px-4 py-1 rounded-md cursor-pointer border border-gray-200 text-teal-700 hover:bg-teal-700 hover:text-white hover:border-teal-700"><i class="fa-regular fa-thumbs-up"></i></button>
+        <button class="mt-4 px-4 py-1 rounded-md cursor-pointer border border-gray-200 text-teal-700 hover:bg-teal-700 hover:text-white hover:border-teal-700">Adopt</button>
+        <button class="mt-4 px-4 py-1 rounded-md cursor-pointer border border-gray-200 text-teal-700 hover:bg-teal-700 hover:text-white hover:border-teal-700">Details</button>
+      </div>
+    </div>
+    `;
+    petsContainer.append(cardDiv);
+  });
+};
+
 //Call loadCategories:
 loadCategories();
+
+//Call loadAllPets:
+loadAllPets();
