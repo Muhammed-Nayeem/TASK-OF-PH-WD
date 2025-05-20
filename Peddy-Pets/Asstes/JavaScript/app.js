515656648d8654c7ae4a1e45068e71f74297260e
Muhammed Nayeem <mohammednayeem808@gmail.com>
2025-05-20 21:49:40 +0600

    Organized all the functionalities

--- Peddy-Pets/Asstes/JavaScript/app.js
+++ Peddy-Pets/Asstes/JavaScript/app.js
@@ -1,85 +1,3 @@
-//Remove Active Class:
-const removeActiveClass = () => {
-  let buttons = document.getElementsByClassName("active");
-  for (let button of buttons) {
-    button.classList.remove("active");
-  }
-};
-
-//Load Categories:
-const loadCategories = async() => {
-  try {
-    let response = await fetch("https://openapi.programming-hero.com/api/peddy/categories");
-    let data = await response.json();
-    displayCategories(data.categories);
-  } catch(e) {
-    console.log(e);
-  }
-};
-
-//Load All Pets:
-const loadAllPets = async() => {
-  try {
-    let response = await fetch("https://openapi.programming-hero.com/api/peddy/pets");
-    let data = await response.json();
-    displayPets(data.pets);
-  } catch(e) {
-    console.log(e);
-  }
-};
-
-//Load Pets By Category Name:
-const loadByCategoryName = async(category_name) => {
-  try {
-    let response = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category_name}`);
-    let data = await response.json();
-    removeActiveClass();
-    let clickedBtn = document.getElementById(`btn-${category_name}`);
-    clickedBtn.classList.add("active");
-    displayPets(data.data);
-  } catch(e) {
-    console.log(e);
-  }
-};
-
-//Load Pets Details By Id:
-const loadByPetId = async(pet_id) => {
-  try {
-    let response = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${pet_id}`);
-    let data = await response.json();
-    displayPetDetails(data.petData);
-  } catch(e) {
-    console.log(e);
-  }
-};
-
-//Store Liked Pets:
-const storeLikedPets = async(pet_id) => {
-  try {
-    let response = await fetch("https://openapi.programming-hero.com/api/peddy/pets");
-    let data = await response.json();
-    let pets = data.pets.filter((pet) => pet.petId === pet_id);
-    displayLikedPets(pets);
-  } catch(e) {
-    console.log(e);
-  }
-};
-
-//Display Liked Pets:
-const displayLikedPets = (pets) => {
-  let likedPetsContainer = document.getElementById("liked-pets");
-  pets.map((pet) => {
-    let likedCard = document.createElement("div");
-    likedCard.classList.add("border", "border-gray-100", "rounded-md", "p-1");
-    likedCard.innerHTML = `
-    <figure>
-      <img src="${pet?.image}" alt="${pet?.pet_name || "pet"}" class="rounded-md" />
-    </figure>
-    `;
-    likedPetsContainer.append(likedCard);
-  });
-};
-
 //Display Categories:
 const displayCategories = (categories) => {
   let categoriesContainer = document.getElementById("categories-container");
@@ -193,6 +111,21 @@ const displayPetDetails = (pet) => {
     </div>
   </div>
   `;
+};
+
+//Display Liked Pets:
+const displayLikedPets = (pets) => {
+  let likedPetsContainer = document.getElementById("liked-pets");
+  pets.map((pet) => {
+    let likedCard = document.createElement("div");
+    likedCard.classList.add("border", "border-gray-100", "rounded-md", "p-1");
+    likedCard.innerHTML = `
+    <figure>
+      <img src="${pet?.image}" alt="${pet?.pet_name || "pet"}" class="rounded-md" />
+    </figure>
+    `;
+    likedPetsContainer.append(likedCard);
+  });
 };
 
 //Call loadCategories:
